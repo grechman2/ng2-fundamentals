d758f01fa79b98d996fa9682ce4f849c6f6c76c1
grechman2 <gretchko.v@gmail.com>
2017-04-09 15:22:09 +0300

    some changes

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,8 +1,24 @@
-import {Component} from '@angular/core'
+import {Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
-    selector: 'event-thumbnail'
+    selector: 'event-thumbnail',
+    template: `
+        <div class="well howerwell thumbnail">
+                 <h2>{{event.name}}</h2>
+                <div>Date: {{event.date}}</div>
+                <div>Time: {{event.time}}</div>
+                <div>Price: \${{event.price}}</div>
+                <div>
+                    <span>Location: {{event.location.address}}</span>
+                    <span class='pad-left'></span>
+                    <span>{{event.location.city}}, {{event.location.country}}</span>
+                </div>
+        </div>
+    `,
+    styles:[`
+        .pad-left { margin-left: 10px; }
+    `]
 })
 export class EventThumbnailComponent{
-    
+ @Input('event') event:any;
 }
