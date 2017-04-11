04c62dfbf82ac5ef9e0d1fd5a7f205e9623e5f01
Vitaliy Grechko <vgrechko@luxoft.com>
2017-04-11 21:26:01 +0300

    nav bar

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
+import { NavBarComponent } from './nav/navbar.component'
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [EventsAppComponent,
-                   EventsListComponent],
+                   EventsListComponent,
+                   NavBarComponent],
     bootstrap: [EventsAppComponent]
 })
 
