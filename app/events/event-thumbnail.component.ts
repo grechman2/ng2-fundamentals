fcec03fe060167efced5d28bb734230718cd2281
Vitaliy Grechko <vgrechko@luxoft.com>
2017-04-13 16:02:05 +0300

    ngClass lessons

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import {Component, Input, Output, EventEmitter } from '@angular/core'
         <div class="well howerwell thumbnail">
                  <h2>{{event.name}}</h2>
                 <div>Date: {{event.date}}</div>
-                <div>Time: {{event.time}}</div>
+                <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time" >
+                    Time: {{event.time}}
+                    <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                    <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                    <span *ngSwitchDefault>(Normal Start)</span>
+                </div>
                 <div>Price: \${{event.price}}</div>
                 <div>
                     <span>Location: {{event.location.address}}</span>
@@ -16,9 +21,24 @@ import {Component, Input, Output, EventEmitter } from '@angular/core'
         </div>
     `,
     styles:[`
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
+        .yellow { color: #FF9933 !important;  }
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
     `]
 })
 export class EventThumbnailComponent{
  @Input('event') event:any;
+
+ getStartTimeClass(){
+     const isEarlyStart = this.event && this.event.time === '8:00 am';
+     if(isEarlyStart){
+         return "green bold";
+    }else{
+        return "";
+    }
+     
+ }
+  
 }
