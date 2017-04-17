9eafc4e4b12ab9f6de4cb8506eb7d6cbf71300b8
Vitaliy G <gretchko.v@gmail.com>
2017-04-17 17:52:06 +0300

    add routes

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -3,25 +3,10 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'events-app',
-    template: 
-    `
-    <nav-bar></nav-bar>
-    <events-list></events-list>
-    `,
+    template: `
+        <nav-bar></nav-bar>
+        <router-outlet></router-outlet>
+        `,
 })
 export class EventsAppComponent {
-    event = {
-        id: 1,
-        name: 'Angular Connect',
-        date: '9/26/2036',
-        time: '10:00 am',
-        price: 599.99,
-        imageUrl: '/app/assets/images/angularconnect-shield.png',
-        location:{
-            adress: '1057 DT',
-            city: 'London',
-            country: 'England'
-        }
-    }
-
 }
