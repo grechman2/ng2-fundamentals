ae77bd541ff542f68a5abb297576146da0406f10
Vitaliy Grechko <vgrechko@luxoft.com>
2017-04-22 13:30:50 +0300

    explore route guardians

--- app/app.module.ts
+++ app/app.module.ts
@@ -25,8 +25,21 @@ import { appRoutes } from './routes'
                    CreateEventComponent,
                    Error404Component,
  		           NavBarComponent],
-    providers: [ToastrService, EventService, EventRouteActivator ],
+    providers: [ToastrService,
+                EventService,    
+                EventRouteActivator,
+                {
+                    provide: 'canDeactivateCreateEvent',
+                    useValue: checkDirtyState
+                }
+                ],
     bootstrap: [EventsAppComponent]
 })
 
 export class AppModule {}
+
+function checkDirtyState(component:CreateEventComponent){
+    if(component.isDirty)
+        return window.confirm("You have not saved this event, do you really want to cancel");
+    return true;
+}
